d7d2d558d908127325096e95490eed9e51fc5aa2
Adam <aberesdeak@autoscout24.com>
2016-11-14 16:01:36 +0100

    basic offline functionality for the service worker + simple testpage for development

--- bin/generate-manifests.js
+++ bin/generate-manifests.js
@@ -20,26 +20,31 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const path = require('path');
 
+const createManifest = (description) => {
+    return {
+        name: 'AutoScout24',
+        short_name: "AutoScout24",
+        start_url: '/?ipl=home-screen&ipc=launch',
+        description,
+        orientation: 'any',
+        display: 'standalone',
+        icons: [{
+          src: "https://www.autoscout24.de/assets/external/home/1518/images/favicon/favicon-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        }],
+        theme_color: '#FF7500',
+        background_color: '#003468'
+    };
+};
+
 urls.forEach(url => {
     axios.get(url).then(res => {
         const tld = url.split('.').pop().replace('/', '');
         const $ = cheerio.load(res.data);
-        const manifest = {
-            name: 'AutoScout24',
-            short_name: "AutoScout24",
-            start_url: '/?ipl=home-screen&ipc=launch',
-            description: $('meta[name=description]').attr('content'),
-            orientation: 'any',
-            display: 'standalone',
-            icons: [{
-              src: "https://www.autoscout24.de/assets/external/home/1518/images/favicon/favicon-192x192.png",
-              sizes: "192x192",
-              type: "image/png"
-            }],
-            theme_color: '#FF7500',
-            background_color: '#003468'
-        };
-
-        fs.writeFileSync(path.resolve(process.cwd(), `manifest.${tld}.json`), JSON.stringify(manifest, null, 4));
+        const manifest = createManifest($('meta[name=description]').attr('content'));
+        fs.writeFileSync(`manifest.${tld}.json`, JSON.stringify(manifest, null, 4));
     });
 });
+
+fs.writeFileSync(`manifest.localhost.json`, JSON.stringify(createManifest('Test description'), null, 4));
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,30 @@
-console.log('Hello Service Worker!');
+self.addEventListener('install', event => {
+    const tld = location.hostname.split('.').pop();
+    var offlineRequest = new Request(`offline.${tld}.html`);
+
+    event.waitUntil(
+        fetch(offlineRequest).then(function(response) {
+            return caches.open('offline').then(function(cache) {
+                return cache.put(offlineRequest, response);
+            });
+        })
+    );
+
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method === 'GET' && !/\.(css|js|png|jpg|jpeg|svg)/i.test(event.request.url)) {
+        // Only provide fallback for HTML content
+        const tld = location.hostname.split('.').pop();
+        const htmlFetchWithFallback = fetch(event.request).catch(function() {
+            return caches.match(`offline.${tld}.html`);
+        });
+
+        event.respondWith(htmlFetchWithFallback);
+    }
+});
