12885bb1476a7925d179e21c633bdc39b41d25b7
Andrei Solovev <andrei.solovev@scout24.com>
2017-03-03 14:19:33 +0100

    replace es6 syntax to old one

--- service-worker/serviceworker.js
+++ service-worker/serviceworker.js
@@ -1,27 +1,27 @@
-self.addEventListener('install', event => {
-    const tld = location.hostname.split('.').pop();
+'use strict';
+
+self.addEventListener('install', function (event) {
+    var tld = location.hostname.split('.').pop();
     var offlineRequest = new Request('/service-worker/offline-pages/offline.' + tld + '.html');
 
-    event.waitUntil(
-        fetch(offlineRequest).then(function(response) {
-            return caches.open('offline').then(function(cache) {
-                return cache.put(offlineRequest, response);
-            });
-        })
-    );
+    event.waitUntil(fetch(offlineRequest).then(function (response) {
+        return caches.open('offline').then(function (cache) {
+            return cache.put(offlineRequest, response);
+        });
+    }));
 
     event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function (event) {
     event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+self.addEventListener('fetch', function (event) {
+    if (event.request.mode === 'navigate' || event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')) {
         // Only provide fallback for HTML content
-        const tld = location.hostname.split('.').pop();
-        const htmlFetchWithFallback = fetch(event.request).catch(function() {
+        var tld = location.hostname.split('.').pop();
+        var htmlFetchWithFallback = fetch(event.request).catch(function () {
             return caches.match('/service-worker/offline-pages/offline.' + tld + '.html');
         });
 
