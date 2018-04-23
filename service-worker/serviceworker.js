c37fc75483319b614f0b9b02b72e58faae554984
bdadam <me@bdadam.com>
2018-04-24 01:51:44 +0200

    cleaned up old idb implementation for offline tracking

--- service-worker/serviceworker.js
+++ service-worker/serviceworker.js
@@ -45,58 +45,12 @@ self.addEventListener('fetch', function (event) {
     }
 });
 
-const readPageviewsFromIdb = () => new Promise((resolve, reject) => {
-    const request = indexedDB.open('offline-tracking', 1);
-    request.onsuccess = function(event) {
-        try {
-            const db = event.target.result;
-            const transaction = db.transaction(['pageviews'], 'readwrite');
-            const objectStore = transaction.objectStore('pageviews');
-
-            const events = [];
-            const cur = objectStore.openCursor();
-            cur.onsuccess = event => {
-                const cursor = event.target.result;
-                if (cursor) {
-                    events.push(cursor.value);
-                    cursor.continue();
-                } else {
-                    resolve(events);
-                }
-            };
-
-            cur.onerror = err => reject(err);
-        } catch(ex) {
-            reject(ex);
-        }
-    };
-
-    request.onerror = err => reject(e);
-});
-
-const deleteAllPageviews = pageviews => new Promise((resolve, reject) => {
-    console.log('CLEAR;');
-    try {
-        const request = indexedDB.open('offline-tracking', 1);
-        request.onsuccess = function(event) {
-            const db = event.target.result;
-            const transaction = db.transaction(['pageviews'], 'readwrite');
-            const objectStore = transaction.objectStore('pageviews');
-            objectStore.clear();
-            resolve();
-        };
-    } catch(ex) {
-        reject(ex);
-    }
-});
-
 self.addEventListener('sync', function(event) {
     const tld = location.hostname.split('.').pop();
 
     if (event.tag.startsWith('offline-tracking-')) {
         const pv = JSON.parse(event.tag.replace('offline-tracking-', ''));
         const obj = {
-            XXXX: 1,
             v: 1,
             aip: 1,
             t: 'pageview',
@@ -118,45 +72,8 @@ self.addEventListener('sync', function(event) {
             body
         });
 
-        console.log(event.tag);
-
         event.waitUntil(prom);
-        // console.log(pv);
     }
-    
-    // if (event.tag === 'offline-tracking') {
-
-    //     const prom = readPageviewsFromIdb().catch(e => []).then(pageviews => {
-    //         const pvsToBeTracked = pageviews.filter(pv => pv.date > Date.now() - 4*60*60*1000).filter((pv, idx) => idx < 20);
-    //         const body = pvsToBeTracked.map(pv => {
-
-    //             const obj = {
-    //                 v: 1,
-    //                 aip: 1,
-    //                 t: 'pageview',
-    //                 tid: getTidForTld(tld),
-    //                 dp: `/vp-${tld}/all/all/offline`,
-    //                 dt: `vp-${tld}/all/all/offline`,
-    //                 cd6: `vp-${tld}/all/all/offline`,
-    //                 cid: pv.ga.cid,
-    //                 sr: pv.ga.sr,
-    //                 vp: pv.ga.vp,
-    //                 cd40: 'tatsu',
-    //                 z: (Math.random() * 1000000) | 0,
-    //                 qt: Date.now() - pv.date
-    //             };
-
-    //             return Object.keys(obj).map(key => `${key}=${obj[key]}`).join('&');
-    //         }).join('\n');
-
-    //         return fetch('https://www.google-analytics.com/collect', {
-    //             method: 'POST',
-    //             body
-    //         }).then(() => deleteAllPageviews());
-    //     });
-
-    //     event.waitUntil(prom);
-    // }
 });
 
 function getTidForTld(tld) {
